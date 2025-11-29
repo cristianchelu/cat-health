52edf309d015282c08fd664f452244bb7fb9a334
cristianchelu@gmail.com <cristianchelu@gmail.com>
2025-11-29 20:00:25 +0200

    chore: fix workbox config

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -96,7 +96,7 @@ export default defineConfig({
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        globPatterns: ['**/*.{js,css,html,svg}', 'icons/*.png'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
